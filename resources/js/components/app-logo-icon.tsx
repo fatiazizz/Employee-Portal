18a65028d241bc4b69846570cf68f413ec548ee2
fAziz1985 <fatemeh.azizzadeh1985@gmail.com>
2026-02-02 12:45:32 -0330

    Push documents

--- resources/js/components/app-logo-icon.tsx
+++
@@ -1,7 +0,0 @@
-import { SVGAttributes } from 'react';
-
-export default function AppLogoIcon(props: SVGAttributes<SVGElement>) {
-    return (
-        <img src='/logo.png' className='w-full h-full'></img>
-    );
-}
